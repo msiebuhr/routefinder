62964c2649efd0f96cb982feb1bd2d951d66d7b6
Morten Siebuhr <sbhr@sbhr.dk>
2015-08-05 22:18:28 +0200

    More and better docs.

--- routefinder.go
+++ routefinder.go
@@ -8,13 +8,16 @@ import (
 	"strings"
 )
 
+// Routes represents an ordered table of URL-like route templates, allowing
+// reverse lookup of these into the original template along with parsed-out
+// variables.
 type Routes []struct {
 	name string
 	re   regexp.Regexp
 }
 
-// Constructs a Route-table from the given templates. Everything in the
-// template from a : to the following / is considered a variable.
+// NewRoutefinder constructs a Route-table from the given templates. Everything
+// in the template from a : to the following / is considered a variable.
 func NewRoutefinder(templates ...string) (Routes, error) {
 	converterRegex, err := regexp.Compile(":[^/]+")
 
@@ -50,7 +53,7 @@ func NewRoutefinder(templates ...string) (Routes, error) {
 	return routes, nil
 }
 
-// Look up the given path in the available Routes, first-match-wins.  A match
+// Lookup the given path in the available Routes, first-match-wins.  A match
 // will return the original template string along with a map of the parsed-out
 // variables. Lookup returns empty values, if no match is found.
 func (r Routes) Lookup(path string) (string, map[string]string) {
